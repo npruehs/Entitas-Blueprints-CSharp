d1b2582096c33f3ff1a4907efde966d4e4622e78
Nick Prühs <dev@npruehs.de>
2016-02-03 23:54:10 +0100

    ADDED: Blueprints - Unique id to use in dictionaries.

--- Source/Entitas.Blueprints/Entitas.Blueprints/Blueprint.cs
+++ Source/Entitas.Blueprints/Entitas.Blueprints/Blueprint.cs
@@ -12,6 +12,12 @@
         public IEnumerable<int> ComponentTypes { get; set; }
 
         /// <summary>
+        ///   Unique identifier of this blueprint.
+        ///   Used in dictionaries to improve lookup performance.
+        /// </summary>
+        public string Id { get; set; }
+
+        /// <summary>
         ///   Values to initialize the component properties with.
         /// </summary>
         public IDictionary<string, object> PropertyValues { get; set; }
